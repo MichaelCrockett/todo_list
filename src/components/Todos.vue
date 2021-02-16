d6b826a1ae27b29705876514a05f1bb94bf7f7ac
Michael Crockett <mike.crockett76@gmail.com>
2021-02-16 19:28:19 +0000

    added Axios, UUID and JSON Placeholder stuff to enhance REST behavuiour

---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="">
+    <div v-for="todo in todos" :key="todo.id" class="">
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+export default {
+  name: "Todos",
+  components: {
+    TodoItem
+  },
+  props: ["todos"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
